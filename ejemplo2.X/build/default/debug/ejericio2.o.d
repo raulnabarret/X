d6a7c632549fdfa567aa4ed11d02313b1ee24622
Raul Nabarret <raulnabarret@gmail.com>
2015-01-20 18:12:01 -0600

    Ejemplo 2
    Copiamos el código para la suma de dos números binarios. Sin errores.
    Compila

--- ejemplo2.X/build/default/debug/ejericio2.o.d
+++ ejemplo2.X/build/default/debug/ejericio2.o.d
@@ -1,3 +1,3 @@
-build/default/production/ejericio2.o:  \
+build/default/debug/ejericio2.o:  \
 	/Users/Admin/Desktop/X/ejemplo2.X/ejericio2.asm  \
 
--- ejemplo2.X/ejericio2.asm
+++ ejemplo2.X/ejericio2.asm
@@ -1,7 +1,23 @@
-        list p=16f84    ;microchip
+;Este programa suma 0001 y 0010 en binario.
+
+        list p=16f84     ;microchip
 
 suma1   equ     0X0C    ;dirección hexadecimal del operando1
 result  equ     0X0E    ;dirección en Hexadecimal
 
+;------------------------------------------------
+
+reset   org     0       ;el vector de reset es la dirección 00
+        goto    inicio  ;se salta al inicio del programa
+        org     5       ;el programa inicia en la dirección
+
+
+inicio  movlw   b'0001' ;se carga el 0001 binario
+        movwf   suma1   ;se carga el registro w en la dirección de memoria de suma1
+        movlw   b'0010' ;se carga el 0010 en binario
+        addwf   suma1,0 ;se suma el valor en W y suma1, y el resultado se queda en W
+        movwf   result   ;se carga el registro W en la dirección de memoria de result
+
+        end
 
 
